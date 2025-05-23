007b16c62297c9613bca91d76fb9ae8644562dde
name <cya.dubois@gmail.com>
2025-05-23 15:02:19 +0200

    ok5

--- creation_covoiturage.sql
+++ creation_covoiturage.sql
@@ -1,58 +1,75 @@
 --
--- base de données: 'autoecole'
+-- base de données: 'Covoiturage'
 --
-create database if not exists autoecole default character set utf8 collate utf8_general_ci;
-use autoecole;
+create database if not exists covoiturage default character set utf8 collate utf8_general_ci;
+use covoiturage;
 -- --------------------------------------------------------
 -- creation des tables
 
 set foreign_key_checks =0;
 
--- table client
-drop table if exists client;
-create table client (
-	cli_id int not null auto_increment primary key,
-    cli_nom varchar(100) not null 
+-- table utilisateur    
+drop table if exists utilisateur;
+create table utilisateur (
+    uti_id int not null auto_increment primary key,    
+    uti_nom varchar (1000) not null,
+    uti_prenom varchar (1000) not null,
+    uti_adresse_mail varchar (1000) not null,
+    uti_mdp varchar (1000) not null,
+    uti_profil int not null
 )engine=innodb;
 
--- table moniteur
-drop table if exists moniteur;
-create table moniteur (
-	mon_id int not null auto_increment primary key,
-    mon_nom varchar(100) not null 
+-- table trajet
+drop table if exists trajet;
+create table trajet (
+    - traj_id int not null auto_increment primary key,
+    - traj_depart varchar(50) not null ,
+    - traj_destination varchar(50) not null ,
+    - traj_moyen de transport varchar(50) not null ,
+    - traj_date_heure_départ date time not null ,
+    - traj_date_heure_arrivée date time not null ,
+    - traj_arrêt_intermédiaire varchar(50) not null ,
+    - traj_date_creation date not null ,
 )engine=innodb;
 
--- table voiture
-drop table if exists voiture;
-create table voiture (
-	voi_id int not null auto_increment primary key,
-    voi_mat varchar(100) not null 
+-- table profil    
+drop table if exists profil;
+create table profil (
+    prof_id int not null auto_increment primary key,    
+    prof_type varchar (100) not null
 )engine=innodb;
 
--- table lecon
-drop table if exists lecon;
-create table lecon (
-	lec_id int not null auto_increment primary key,
-    lec_date date not null ,
-    lec_hdebut  time not null ,
-    lec_hfin  time not null ,
-    lec_type  varchar(50) not null ,
-    lec_moniteur int not null,
-    lec_voiture int
+-- table proposer    
+drop table if exists proposer;
+create table proposer (
+    pro_id int not null auto_increment primary key,    
+    pro_utilisateur int not null,
+    pro_trajet int not null,
 )engine=innodb;
 
--- table recevoir
-drop table if exists recevoir;
-create table recevoir (
-	rec_client int not null,
-    rec_lecon  int not null,    
-    primary key (rec_lecon,rec_client)
+-- table inscrire    
+drop table if exists inscrire;
+create table inscrire (
+    ins_id int not null auto_increment primary key,    
+    ins_utilisateur int not null,
+    ins_trajet int not null,
+)engine=innodb;
+
+-- table posseder    
+drop table if exists posseder;
+create table posseder (
+    pos_id int not null auto_increment primary key,    
+    pos_utilisateur int not null,
+    pos_profil int not null,
 )engine=innodb;
 
 set foreign_key_checks =1;
 
 -- contraintes
-alter table lecon add constraint cs1 foreign key (lec_moniteur) references moniteur(mon_id);
-alter table lecon add constraint cs2 foreign key (lec_voiture) references voiture(voi_id);
-alter table recevoir add constraint cs3 foreign key (rec_client) references client(cli_id);
-alter table recevoir add constraint cs4 foreign key (rec_lecon) references lecon(lec_id);
+alter table utilisateur add constraint cs1 foreign key (uti_profil) references profil(prof_id);
+alter table proposer add constraint cs2 foreign key (pro_utilisateur) references utilisateur(uti_id);
+alter table proposer add constraint cs3 foreign key (pro_trajet) references trajet(traj_id);
+alter table inscrire add constraint cs4 foreign key (pro_utilisateur) references utilisateur(uti_id);
+alter table inscrire add constraint cs5 foreign key (pro_trajet) references trajet(traj_id);
+alter table posseder add constraint cs6 foreign key (pos_utilisateur) references utilisateur(uti_id);
+alter table posseder add constraint cs7 foreign key (pos_profil) references profil(prof_id);
