ab0c75827c89bf068a0c6c3f5e8ebd75f6730ea1
name <cya.dubois@gmail.com>
2025-05-27 17:20:38 +0200

    OK6

--- creation_covoiturage.sql
+++ creation_covoiturage.sql
@@ -22,29 +22,22 @@ create table utilisateur (
 -- table trajet
 drop table if exists trajet;
 create table trajet (
-    - traj_id int not null auto_increment primary key,
-    - traj_depart varchar(50) not null ,
-    - traj_destination varchar(50) not null ,
-    - traj_moyen de transport varchar(50) not null ,
-    - traj_date_heure_départ date time not null ,
-    - traj_date_heure_arrivée date time not null ,
-    - traj_arrêt_intermédiaire varchar(50) not null ,
-    - traj_date_creation date not null ,
+    tra_id int not null auto_increment primary key,
+    tra_depart varchar(100) not null,
+    tra_destination varchar(50) not null,
+    tra_moyen_de_transport varchar(50) not null,
+    tra_date_heure_depart date time not null,
+    tra_date_heure_arrivee date time not null,
+    tra_arret_intermediaire varchar(50) not null,
+    tra_date_creation date not null,
+    tra_fournisseur int not null
 )engine=innodb;
 
 -- table profil    
 drop table if exists profil;
 create table profil (
-    prof_id int not null auto_increment primary key,    
-    prof_type varchar (100) not null
-)engine=innodb;
-
--- table proposer    
-drop table if exists proposer;
-create table proposer (
     pro_id int not null auto_increment primary key,    
-    pro_utilisateur int not null,
-    pro_trajet int not null,
+    pro_type varchar (100) not null
 )engine=innodb;
 
 -- table inscrire    
@@ -55,21 +48,10 @@ create table inscrire (
     ins_trajet int not null,
 )engine=innodb;
 
--- table posseder    
-drop table if exists posseder;
-create table posseder (
-    pos_id int not null auto_increment primary key,    
-    pos_utilisateur int not null,
-    pos_profil int not null,
-)engine=innodb;
-
 set foreign_key_checks =1;
 
 -- contraintes
-alter table utilisateur add constraint cs1 foreign key (uti_profil) references profil(prof_id);
-alter table proposer add constraint cs2 foreign key (pro_utilisateur) references utilisateur(uti_id);
-alter table proposer add constraint cs3 foreign key (pro_trajet) references trajet(traj_id);
-alter table inscrire add constraint cs4 foreign key (pro_utilisateur) references utilisateur(uti_id);
-alter table inscrire add constraint cs5 foreign key (pro_trajet) references trajet(traj_id);
-alter table posseder add constraint cs6 foreign key (pos_utilisateur) references utilisateur(uti_id);
-alter table posseder add constraint cs7 foreign key (pos_profil) references profil(prof_id);
+alter table utilisateur add constraint cs1 foreign key (uti_profil) references profil(pro_id);
+alter table trajet add constraint cs2 foreign key (tra_fournisseur) references utilisateur(uti_id);
+alter table inscrire add constraint cs4 foreign key (ins_utilisateur) references utilisateur(uti_id);
+alter table inscrire add constraint cs5 foreign key (ins_trajet) references trajet(tra_id);
